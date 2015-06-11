05cd5d6438747077cde0b438aaa16cddf2ea9ca5
yesica <yesicargueta@gmail.com>
2015-06-10 19:50:21 -0600

    Clases de operaciones, Interfaz Operacion

---
+++ src/Operaciones/Multiplicacion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Operaciones;
+import analisiscalculadora.Operacion;
+/**
+ *
+ * @author Cliente
+ */
+public class Multiplicacion implements Operacion{
+    
+    /**
+     * Metodo que esta en la interfaz
+     */
+    
+     public void Operacion(){
+        ;
+    }
+    
+}
---
+++ src/analisiscalculadora/Operacion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analisiscalculadora;
+
+/**
+ *
+ * @author Cliente
+ */
+public interface Operacion {
+    /**
+     * Metodo para la interfaz Operacion
+     */
+    public void Operacion();
+}
