4fcb086d2ad22d219cf1f1f72d1f085a18b7c72b
dvader33 <vhader33.dr@gmail.com>
2015-06-13 12:41:11 -0600

    agregando clases

---
+++ src/Estructura/Calculadora.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Estructura;
+
+/**
+ *
+ * @author Vader33
+ */
+public class Calculadora {
+ 
+    public Operacion TipoOperacion;
+    private double resultado;
+    
+    
+    
+}
