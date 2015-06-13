6986ace0c31c145e056c7b7465a85c68e28a3e60
dvader33 <vhader33.dr@gmail.com>
2015-06-13 13:16:24 -0600

    Estructura de clase calculadora

--- src/Estructura/Calculadora.java
+++ src/Estructura/Calculadora.java
@@ -14,6 +14,19 @@ public class Calculadora {
     public Operacion TipoOperacion;
     private double resultado;
     
+    public void Calculadora(){
+        this.resultado=0;
+    }
     
+    public void setTipoOperacion (Operacion tipo){
+        this.TipoOperacion=tipo;
+    }
     
-}
+    public void EjecutarOperacion(){
+       resultado=this.TipoOperacion.Operar();
+    }
+    
+    public void ImprimirResultado(){
+            System.out.println("El resultado es: "+this.resultado);
+    }
+} 
