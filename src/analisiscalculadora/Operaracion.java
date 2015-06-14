83dcf7ebf472ef63414adfc19cd35b893407cf97
Lester Meneses <lesterjosue1@gmail.com>
2015-06-14 09:22:57 -0600

    REVISANDO COMENTARIOS

---
+++ src/analisiscalculadora/Operaracion.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analisiscalculadora;
+
+/**
+ *
+ * @author Geek
+ */
+public interface Operaracion {
+    
+    Number operar(); // SIN CUERPO, POR ESO SE LE PUSO PUNTO Y COMA.
+    
+}
